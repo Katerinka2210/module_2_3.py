172568e6bd1d3b94218074b91fd039166822737f
Katerinka2210 <stavalove8739@gmail.com>
2024-10-25 16:49:51 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,23 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] > 0:
+        print(my_list[index])
+        index += 1
+    elif my_list[index] == 0:
+        index += 1 # увеличиваем индекс на 1, если встречаем 0
+        continue
+    else:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
